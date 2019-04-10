cf264b14db92aa1919e724c8224385248390113b
Lizbethluna <lizbeth.luna.21@gmail.com>
2019-04-10 11:04:07 -0500

    Se agregan cambios para verificacion en produccion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     </header>
 
     <section class="jumbotron descripcion-home text-center">
-        <div class="container container-text">
+        <div class="container container-text py-4">
             <img src="{{asset('img/aldea.gif')}}" class="mb-3" width="30">
             <p class="lead text-muted text-principal" 
                 data-0="position: relative; opacity: 0.2; top: 20px;" 
@@ -28,7 +28,7 @@
             <div class="col-md-6">
                 <img src="{{asset('img/elementos/elemento1.svg')}}" class="img-hoja img1" data-0="left: -20px; top: -150px;" data-500="left: 50px; top: -5px;">
                 <div class="content-contacto-left">
-                    <div class="content-textos center-middle-table pt-5" data-300="opacity: 0;" data-700="opacity: 1;">
+                    <div class="content-textos center-middle-table" data-300="opacity: 0;" data-700="opacity: 1;">
                         <p class="texto1">¿Quieres más informes ó deseas alguna cotización? </p>
                         <p class="texto2">Escríbenos.</p>
                     </div>
@@ -36,7 +36,7 @@
                 <img src="{{asset('img/elementos/elemento2.svg')}}" class="img-hoja img2" data-0="right: -30px; bottom: -150px;" data-700="right: 50px; bottom: -5px;">
             </div>
             <div class="col-md-6">
-                <form id="contactoForm" name="contactoForm" data-300="opacity: 0;" data-700="opacity: 1;">
+                <form id="contactoForm" name="contactoForm" class="mt-3" data-300="opacity: 0;" data-700="opacity: 1;">
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <input type="text" class="form-control" id="nombre" name="nombre" ng-model="contacto.nombre" placeholder="Nombre">
