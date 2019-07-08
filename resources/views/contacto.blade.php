968a5c66aaf4292ad6d78fbe2cf657cfdcdcf697
OmarAbaroa <alvaro1295@hotmail.com>
2019-07-08 14:07:56 -0500

    link a maps

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -57,7 +57,7 @@
                 <div class="container-info container-info-left float-right" data-aos="fade-right" data-aos-duration="1000">
                     <img src="{{asset('img/iconos/ubication.gif')}}" class="mb-4" width="60"/>
                     <h6>UBICACIÓN</h6>
-                    <p class="address">Camino a Corralejo s/n Entre la deportiva y Cbtis. Cortazar, Gto.</p>
+                    <p class="address"><a href="https://www.google.com/maps/@20.492564,-100.968681,18z" target="_blank">Camino a Corralejo s/n Entre la deportiva y Cbtis. Cortazar, Gto.</a></p>
                 </div>
             </div>
             <div class="col-sm-6 col-md-6">
